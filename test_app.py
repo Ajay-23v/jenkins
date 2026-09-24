54a63fbb00e97144f443457482671d87e47ca7e0
Prethish16 <prethish.s2024@vitstudent.ac.in>
2026-09-24 10:49:13 +0530

    Add failing test case to count_negatives

--- test_app.py
+++ test_app.py
@@ -14,6 +14,7 @@ def test_sum_list(numbers, expected):
     ([], 0),
     ([1, 2, 3], 0),
     ([-1, 2, -3], 2)
+    ([-5, -10], 1)
 ])
 def test_count_negatives(numbers, expected):
     assert count_negatives(numbers) == expected
