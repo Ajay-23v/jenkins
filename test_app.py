3e6fc6003b9c408fdb0c1278a9a4d7f4ec5d00f7
Prethish16 <prethish.s2024@vitstudent.ac.in>
2026-09-24 10:29:20 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from App import sum_list, count_negatives
+
+@pytest.mark.parametrize("numbers, expected", [
+    ([], 0),
+    ([1, 2, 3], 6),
+    ([-1, -2, -3], -6),
+    ([2.5, 2.5], 5.0)
+])
+def test_sum_list(numbers, expected):
+    assert sum_list(numbers) == expected
+
+@pytest.mark.parametrize("numbers, expected", [
+    ([], 0),
+    ([1, 2, 3], 0),
+    ([-1, 2, -3], 2)
+])
+def test_count_negatives(numbers, expected):
+    assert count_negatives(numbers) == expected
