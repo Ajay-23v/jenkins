c9ee957ac389c4fbc94f79b8fc360290bc375b74
Prethish16 <prethish.s2024@vitstudent.ac.in>
2026-09-24 10:52:41 +0530

    Add failing test case to count_negatives

--- test_app.py
+++ test_app.py
@@ -13,7 +13,7 @@ def test_sum_list(numbers, expected):
 @pytest.mark.parametrize("numbers, expected", [
     ([], 0),
     ([1, 2, 3], 0),
-    ([-1, 2, -3], 2)
+    ([-1, 2, -3], 2),
     ([-5, -10], 1)
 ])
 def test_count_negatives(numbers, expected):
